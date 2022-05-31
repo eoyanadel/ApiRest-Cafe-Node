c5e57fb0e6548b106e838faa09e68b90e14053a4
Esteban Oyanadel <eoc.ingecomp@gmail.com>
2022-05-30 21:52:32 -0400

    Google Sign In added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@ const { request, response } = require("express");
 const Usuario = require('../models/usuario.model');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async(req = request, res = response) => {
 
@@ -48,6 +49,59 @@ const login = async(req = request, res = response) => {
 };
 
 
+
+const googleSignIn = async(req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+        
+        const { nombre, img, correo } = await googleVerify(id_token);
+        
+        let usuario = await Usuario.findOne({ correo });
+
+        if (!usuario) {
+            // Crear usuario
+            const data = {
+                nombre,
+                correo,
+                password: 'abc123',
+                img,
+                rol: 'USER_ROLE',
+                google: true
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador - Usuario bloqueado'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT(usuario.id);
+
+
+        res.json({
+            usuario,
+            token
+        });
+
+    } catch (error) {
+
+        return res.status(404).json({
+            ok: false,
+            msg: 'El Token no se pudo verificar'
+        });
+
+    }
+};
+
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 };
